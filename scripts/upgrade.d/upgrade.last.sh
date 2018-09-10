cc7d3311e93db91f4d6dda7d38175f3abde7a279
frju365 <win10@tutanota.com>
2018-09-10 18:24:03 +0200

    Update upgrade.last.sh

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Last available nextcloud version
-next_version="13.0.5"
+next_version="14.0.0"
 
 # Nextcloud tarball checksum sha256
-nextcloud_source_sha256="a110d32849259ab79813af3078123a09057fc659ee414e5f3ed75451ec9e80ea"
+nextcloud_source_sha256="f965c14286e7aabbfe49c947d86af59597af302c35d10e0b5440e7e6c53b8f47"
 
 # Patch nextcloud files only for the last version
 cp -a ../sources/patches_last_version/* ../sources/patches
